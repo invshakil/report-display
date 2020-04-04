65f471b2e1809fdcd433dade0ac091f94b7e092f
shakil <inverse.shakil@gmail.com>
2020-04-04 21:45:56 +0600

    environment setup

--- vue.config.js
+++ vue.config.js
@@ -9,17 +9,18 @@
 
 
 module.exports = {
-  publicPath: '/',
-  transpileDependencies: [
-    'vue-echarts',
-    'resize-detector'
-  ],
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    }
-  }
+    publicPath: '/',
+    transpileDependencies: [
+        'vue-echarts',
+        'resize-detector'
+    ],
+    configureWebpack: {
+        optimization: {
+            splitChunks: {
+                chunks: 'all'
+            }
+        }
+    },
+    chainWebpack: config => config.resolve.symlinks(false)
 }
 
